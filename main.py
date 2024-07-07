56fba3156c232caf9f4ac5e86563e411c164bae9
avikeseal <avikeseal@gmail.com>
2024-07-07 15:56:07 -0400

    testing...

--- main.py
+++ main.py
@@ -6,11 +6,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 #decorator - to make this accessible (slash is the path we want to access, the default route)
-@app.route("/")
 
-#function for route - contains data that we want user to have access to when they reach this route
-def home():
-    return "Home"
 
 #to run our flask server
 if __name__== "__main__":
